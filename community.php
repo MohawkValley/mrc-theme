54186a7f3c0ff98c174c1455eae3d7a943a53643
Paul Lee <paul_lee@zoho.com>
2017-04-17 20:01:24 -0400

    Adding theme with its minimal customizations. The initial test of adding classes the the wrapper elements by modifying the 'content-front-page-panels.php' file is excluded from this repository using the gitignore file, if it worked correctly.

---
+++ community.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Template Name: Facebook Feed Page
+ * Description: A normal page with the Facebook API include code for the church's Facebook webpage. Intended to show a timeline feed as a section on the homepage.
+ */
+
+get_header(); ?>
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<div class="wrap">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php
+			while ( have_posts() ) : the_post();
+
+				get_template_part( 'template-parts/page/content', 'page' );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			endwhile; // End of the loop.
+			?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+</div><!-- .wrap -->
+
+<?php get_footer();
