7dfd4c5415a19a1906e3dbca4880b42690ce03d5
Paul Lee <paul_lee@zoho.com>
2017-04-21 16:18:20 -0400

    Added conditional that successfully outputs a new class 'content-wide' on the parent 'entry-content' <div> on the Content page.

--- template-parts/page/content-page.php
+++ template-parts/page/content-page.php
@@ -17,7 +17,17 @@
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 		<?php twentyseventeen_edit_link( get_the_ID() ); ?>
 	</header><!-- .entry-header -->
-	<div class="entry-content">
+	<?php
+		/* This helper class 'content-wide' forces the 'entry-content' div to 100% width so that different elements can be placed alongside each other.
+		   Some pages need the class only when concatenated on the homepage, which comes from the file 'content-front-page-panels.php'. The Contact page
+		   needs the class both when concatenated on the homepage and when viewed as an individual page, so its conditional output goes here. */
+		if (is_page('contact')) {
+			echo "<div class=\"entry-content content-wide\">";
+		}
+		else {
+			echo "<div class=\"entry-content\">";
+		}
+	?>
 		<?php
 			the_content();
 
