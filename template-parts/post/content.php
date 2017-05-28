7dbb7f5eaea05bd59d0ded30c789394f56290d48
Paul Lee <paul_lee@zoho.com>
2017-05-28 12:39:24 -0400

    Put a conditional in 'content.php' to output excerpts instead of full posts on the blog home feed.

--- template-parts/post/content.php
+++ template-parts/post/content.php
@@ -66,11 +66,18 @@
   <div class="entry-content">
     <?php
       /* translators: %s: Name of current post */
-      the_content( sprintf(
-        __( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentyseventeen' ),
-        get_the_title()
-      ) );
-
+      if ( is_home() && !is_front_page() ) {
+        the_excerpt( sprintf(
+          __( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentyseventeen' ),
+          get_the_title()
+        ) );
+      }
+      else {
+        the_content( sprintf(
+          __( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentyseventeen' ),
+          get_the_title()
+        ) );
+      }
       wp_link_pages( array(
         'before'      => '<div class="page-links">' . __( 'Pages:', 'twentyseventeen' ),
         'after'       => '</div>',
