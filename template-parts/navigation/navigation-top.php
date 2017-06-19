15e25575ebb5305c0661b4092c848a8442c99315
Paul Lee <paul_lee@zoho.com>
2017-06-19 11:13:46 -0400

    Added conditional to output the normal menu on the frontpage. This doesn't account for static pages that are neither the blog feed page nor the front page, but the website probably doesn't need any of those.

--- template-parts/navigation/navigation-top.php
+++ template-parts/navigation/navigation-top.php
@@ -11,10 +11,23 @@
 ?>
 <nav id="site-navigation" class="main-navigation" role="navigation" aria-label="<?php _e( 'Top Menu', 'twentyseventeen' ); ?>">
 	<button class="menu-toggle" aria-controls="top-menu" aria-expanded="false"><?php echo twentyseventeen_get_svg( array( 'icon' => 'bars' ) ); echo twentyseventeen_get_svg( array( 'icon' => 'close' ) ); _e( 'Menu', 'twentyseventeen' ); ?></button>
-	<?php wp_nav_menu( array(
-		'theme_location' => 'top',
-		'menu_id'        => 'top-menu',
-	) ); ?>
+	<?php
+
+    if ( twentyseventeen_is_frontpage() || ( is_home() && is_front_page() ) ) {
+      wp_nav_menu( array(
+        'theme_location' => 'top',
+        'menu_id'        => 'top-menu',
+      ) );
+    }
+    /* This conidtional structure assumes that the only page other than the front page that will ever be seen is the blog feed page. If the website ever needs to accomodate static pages, these conditionals need to be refined in order to output appropriate menus with appropriate links/anchors for the static content pages. */
+    else {
+      wp_nav_menu( array(
+        'theme_location' => 'top',
+        'menu'           => 'Blog Menu',
+        'menu_id'        => 'top-menu',
+      ) );
+    }
+  ?>
 
 	<?php if ( ( twentyseventeen_is_frontpage() || ( is_home() && is_front_page() ) ) && has_custom_header() ) : ?>
 		<a href="#content" class="menu-scroll-down"><?php echo twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ); ?><span class="screen-reader-text"><?php _e( 'Scroll down to content', 'twentyseventeen' ); ?></span></a>
