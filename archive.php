d94e8ebeb8e0a34f7532f4c80383ab464cc3345b
Paul Lee <paul_lee@zoho.com>
2017-06-25 10:14:14 -0400

    Output the <section> element around the excerpts on archive pages as well as on the main feed page by echoing it directly in 'archive.php'.

--- archive.php
+++ archive.php
@@ -32,12 +32,16 @@ get_header('single'); ?>
 			/* Start the Loop */
 			while ( have_posts() ) : the_post();
 
+				echo "<section class='feed-post-listing'>";
+
 				/*
 				 * Include the Post-Format-specific template for the content.
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 				 */
 				get_template_part( 'template-parts/post/content', get_post_format() );
+
+				echo "</section>";
 
 			endwhile;
 
