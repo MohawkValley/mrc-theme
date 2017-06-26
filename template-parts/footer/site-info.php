2bc3a63a1d23758a18cc6d49e1c424027a325dc5
Paul Lee <paul_lee@zoho.com>
2017-06-26 09:12:27 -0400

    Added a little section in the 'site-info' <div> in the footer, as a child <div>, and styled it based on an assumption of its containing only one <p> element or one <a> element (or probably an <a> within a <p>).

--- template-parts/footer/site-info.php
+++ template-parts/footer/site-info.php
@@ -11,4 +11,7 @@
 ?>
 <div class="site-info">
 	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyseventeen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyseventeen' ), 'WordPress' ); ?></a>
+  <div class="site-closer">
+    <p>Soli Deo Gloria</p>
+  </div>
 </div><!-- .site-info -->
