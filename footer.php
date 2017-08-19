da37577eaa52d7d03f0c3ffa16c37d6e067c4be7
Paul Lee <paul_lee@zoho.com>
2017-08-19 15:30:25 -0400

    Deleted console.log() messages in the embedded JavaScript.

--- footer.php
+++ footer.php
@@ -63,15 +63,11 @@
 ?>
 
 <script>
-	console.log("*** Running <main> tag shortener ***");
 	jQuery(document).ready(function($) {
 		if ($(window).width() > 999) {
 			var container = $('#page');
-			console.log("Container object: "+container.className);
 			var oldHeight = container.height();
-			console.log("Old height: "+oldHeight);
 			var newHeight = oldHeight - 232;
-			console.log("Computed new height: "+newHeight);
 			var newHeightString = newHeight + 'px';
 			container.css('overflow', 'hidden');
 			container.css('max-height', newHeightString);
