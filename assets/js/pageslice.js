e08bd17fc4a16af1654f2c0168a23cc2ecd9f580
Paul Lee <paul_lee@zoho.com>
2017-08-21 18:18:31 -0400

    Fixed a problem where the pageslice code wasn't running until after the page was reloaded, due to some caching thing with the jQuery.ready loading method. (Changed to plain JS window.onload). Also added the selector prefix to the background color statement in the CSS, to avoid modifying posts in feeds.

--- assets/js/pageslice.js
+++ assets/js/pageslice.js
@@ -1,10 +1,10 @@
-jQuery(document).ready(function($) {
-  if ($(window).width() > 999) {
-    var container = $('#page');
+window.onload = function() {
+  if (jQuery(window).width() > 999) {
+    var container = jQuery('#page');
     var oldHeight = container.height();
     var newHeight = oldHeight - 232;
     var newHeightString = newHeight + 'px';
     container.css('overflow', 'hidden');
     container.css('max-height', newHeightString);
   }
-});
+}
