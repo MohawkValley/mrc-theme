d6f4669548e25b6c9bd77c22b232670b604fd378
Paul Lee <paul_lee@zoho.com>
2018-01-02 17:25:44 -0500

    Created a CLI debug mode. For security purposes, all debug related code needs to be removed in a future commit prior to uploading the plugin on a live, Internet-exposed version of the MRC website.

--- mrc-service-autolister/mrc-service-autolister.php
+++ mrc-service-autolister/mrc-service-autolister.php
@@ -6,11 +6,28 @@
 	Author: Strivenword
 */
 
+/* ****************************
+ * CLI Debug mode
+ * Remove all associated debug code before publishing plugin on public website!
+ ************** */
+global $DEBUG_MODE;
+if ($argv[1] === "debug") {
+	$DEBUG_MODE = 1;
+	print("\n............ CLI Debug Mode ............\n\n");
+}
+
 // Security precaution suggested by codex.wordpress.org/Writing_a_Plugin
-defined( 'ABSPATH' ) or die( 'Only internal PHP scripts allowed.' );
+if (!$DEBUG_MODE) { # Remove the condition when removing debug code
+	defined( 'ABSPATH' ) or die( 'Only internal PHP scripts allowed.' );
+}
 
 // Set the default timezone to Eastern Standard Time
 date_default_timezone_set('America/New_York');
+
+if ($DEBUG_MODE) { # Remove when removing debug code
+	$tzone = date_default_timezone_get();
+	print("Default time zone: ".$tzone."\n\n");
+}
 
 // Find today's date
 
